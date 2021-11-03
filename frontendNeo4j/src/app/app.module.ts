25d50539fc15f428ff84165fb28aa33a0a2ea2cb
OttavioBuonomoUSI <91184854+OttavioBuonomoUSI@users.noreply.github.com>
2021-11-03 12:05:25 +0100

    Displaying persons from Neo4j

--- frontendNeo4j/src/app/app.module.ts
+++ frontendNeo4j/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import {FormsModule} from "@angular/forms";
+import {AngularNeo4jModule} from "angular-neo4j";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularNeo4jModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontendNeo4j/src/app/mock-persons.ts
@@ -0,0 +1,11 @@
+import {Person} from "./person";
+
+export const PERSONS: Person[] = [
+  {person_id: 1, person_name: "John", person_surname: "Doe"},
+  {person_id: 2, person_name: "John", person_surname: "Doe"},
+  {person_id: 3, person_name: "John", person_surname: "Doe"},
+  {person_id: 4, person_name: "John", person_surname: "Doe"},
+  {person_id: 5, person_name: "John", person_surname: "Doe"},
+  {person_id: 6, person_name: "John", person_surname: "Doe"},
+  {person_id: 7, person_name: "John", person_surname: "Doe"}
+]
